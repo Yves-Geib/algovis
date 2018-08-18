d3ba688a69c7e380174b1e17c97ada330a7477dd
Yves Geib <yves.geib@web.de>
2018-08-18 10:59:17 +0200

    added the new ahocorasick file with the XML changes

--- AlgoVis Projekt/src/AhoCorasick/AhoCorasick.java
+++ AlgoVis Projekt/src/AhoCorasick/AhoCorasick.java
@@ -1,8 +1,81 @@
-package ahoCorasick;
+/*
+ * AhoCorasick.java
+ * Hannah Drews, Yves Geib, 2018 for the Animal project at TU Darmstadt.
+ * Copying this file for educational purposes is permitted without further authorization.
+ */
+package AhoCorasick;
+
+import generators.framework.Generator;
+import generators.framework.GeneratorType;
 
 import java.util.*;
 
-public class AhoCorasick {
+import algoanim.primitives.generators.Language;
+import generators.framework.properties.AnimationPropertiesContainer;
+import algoanim.animalscript.AnimalScript;
+import algoanim.properties.TextProperties;
+import algoanim.properties.ArrayProperties;
+import algoanim.properties.ArrayMarkerProperties;
+import algoanim.properties.SourceCodeProperties;
+
+public class AhoCorasick implements Generator {
+    private Language lang;
+    private TextProperties textProps;
+    private ArrayProperties arrayProps;
+    private ArrayMarkerProperties arrayMarkerProps;
+    private String[] inputText;
+    private SourceCodeProperties sourceCodeProps;
+
+    public void init() {
+        lang = new AnimalScript("Aho-Corasick algorithm", "Hannah Drews, Yves Geib", 800, 600);
+    }
+
+    public String generate(AnimationPropertiesContainer props, Hashtable<String, Object> primitives) {
+        textProps = (TextProperties) props.getPropertiesByName("textProps");
+        arrayProps = (ArrayProperties) props.getPropertiesByName("arrayProps");
+        arrayMarkerProps = (ArrayMarkerProperties) props.getPropertiesByName("arrayMarkerProps");
+        inputText = (String[]) primitives.get("inputText");
+        sourceCodeProps = (SourceCodeProperties) props.getPropertiesByName("sourceCodeProps");
+
+        return lang.toString();
+    }
+
+    public String getName() {
+        return "Aho-Corasick algorithm";
+    }
+
+    public String getAlgorithmName() {
+        return "Aho-Corasick string matching algorithm";
+    }
+
+    public String getAnimationAuthor() {
+        return "Hannah Drews, Yves Geib";
+    }
+
+    public String getDescription() {
+        return "Aho-Corasick is a string-matching algorithm that locates elements of a finite set of strings within an input text."
+                + "\n";
+    }
+
+    public String getCodeExample() {
+        return "CodeExample";
+    }
+
+    public String getFileExtension() {
+        return "asu";
+    }
+
+    public Locale getContentLocale() {
+        return Locale.ENGLISH;
+    }
+
+    public GeneratorType getGeneratorType() {
+        return new GeneratorType(GeneratorType.GENERATOR_TYPE_SEARCH);
+    }
+
+    public String getOutputLanguage() {
+        return Generator.PSEUDO_CODE_OUTPUT;
+    }
 
 
     //R- alphabet size
@@ -16,44 +89,46 @@ public class AhoCorasick {
 
     //g[][] - goto function(Trie)
 
-    public static int R = 26,M = 500;
-    public static int[] out,f;
+    public static int R = 26, M = 500;
+    public static int[] out, f;
     public static int[][] g;
 
     //Initialize all values in
     //failure function to -1
     //goto function to -1
     //out function to 0 [default when created]
-    public static void init(){
-        out = new int[M];f = new int[M];g = new int[M][R];
+    public static void initialize() {
+        out = new int[M];
+        f = new int[M];
+        g = new int[M][R];
         Arrays.fill(f, -1);
-        for(int[] row:g){
+        for (int[] row : g) {
             Arrays.fill(row, -1);
         }
     }
 
-    public static int buildMatchingMachine(String[] arr,int k){
+    public static int buildMatchingMachine(String[] arr, int k) {
 
         //Initialize step is called
-        init();
+        initialize();
 
         //Initially we just have 0 states
-        int states=1;
+        int states = 1;
 
         //Construct values for goto function same as
         //building a Trie for arr[]
-        for(int i=0;i<k; i++){
+        for (int i = 0; i < k; i++) {
 
-            int currentState=0;
+            int currentState = 0;
             char[] word = arr[i].toCharArray();
 
-            for(int j=0; j<word.length;j++){
+            for (int j = 0; j < word.length; j++) {
 
-                int ch = word[j]-'a';
+                int ch = word[j] - 'a';
 
                 //Allocate a new node (create a new state) if a
                 // node for ch doesn't exist
-                if(g[currentState][ch]==-1){
+                if (g[currentState][ch] == -1) {
                     g[currentState][ch] = states++;
                 }
 
@@ -61,16 +136,16 @@ public class AhoCorasick {
             }
 
             //Add current word in output function
-            out[currentState] |= (1<<i);
+            out[currentState] |= (1 << i);
 
         }
 
         //For all characters which don't have an edge from root
         //(or state 0) in Trie, add a goto edge to state 0 itself
 
-        for(int ch=0; ch<R; ch++){
-            if(g[0][ch]==-1){
-                g[0][ch]=0;
+        for (int ch = 0; ch < R; ch++) {
+            if (g[0][ch] == -1) {
+                g[0][ch] = 0;
             }
         }
 
@@ -80,33 +155,33 @@ public class AhoCorasick {
         Queue<Integer> q = new LinkedList<Integer>();
 
         //Iterate over every possible input
-        for(int ch=0;ch<R;ch++){
+        for (int ch = 0; ch < R; ch++) {
             //All nodes of depth 1 have failure function value as 0
             //For example, in above diagram we move to 0 from states 1 and 3
-            if(g[0][ch]!=0){
-                f[g[0][ch]]=0;
+            if (g[0][ch] != 0) {
+                f[g[0][ch]] = 0;
                 q.add(g[0][ch]);
             }
         }
 
         //Now queue has states 1 and 3
-        while(!q.isEmpty()){
+        while (!q.isEmpty()) {
             //Remove the front state from queue
             int state = q.poll();
 
             //For the removed state, find failure function for all
             //those characters for which goto function is not defined
-            for(int ch=0; ch<R; ch++){
+            for (int ch = 0; ch < R; ch++) {
 
                 //If goto function is defined
-                if(g[state][ch]!=-1){
+                if (g[state][ch] != -1) {
 
                     //Find failure state of removed state
                     int failure = f[state];
 
                     //Find the deepest node labelled by proper suffix
                     //of string from root to current state
-                    while(g[failure][ch]==-1){
+                    while (g[failure][ch] == -1) {
                         failure = f[failure];
                     }
 
@@ -131,12 +206,12 @@ public class AhoCorasick {
     //               [Must be between 0 and no. of states -1, inclusive]
     //nextInput - The next character that enters the machine
 
-    public static int findNextState(int currentState, char nextInput){
+    public static int findNextState(int currentState, char nextInput) {
 
-        int ch= nextInput-'a';
+        int ch = nextInput - 'a';
 
         //If goto function is not defined use failure function
-        while(g[currentState][ch]==-1){
+        while (g[currentState][ch] == -1) {
             currentState = f[currentState];
         }
 
@@ -144,30 +219,30 @@ public class AhoCorasick {
     }
 
     //Finds all occurrences of all array words in text
-    public static void searchWords(String[] arr, int k, String text){
+    public static void searchWords(String[] arr, int k, String text) {
 
         //Preprocess patterns
         //Build machine with goto, failure and output functions
-        buildMatchingMachine(arr,k);
+        buildMatchingMachine(arr, k);
 
         //Initialize the current state
-        int currentState=0;
+        int currentState = 0;
 
         //Traverse the text through the built machine to find all
         //occurrences of words in arr[]
-        for(int i=0; i<text.length(); i++){
+        for (int i = 0; i < text.length(); i++) {
 
-            currentState = findNextState(currentState,text.charAt(i));
+            currentState = findNextState(currentState, text.charAt(i));
 
             //If match not found, move to next state
-            if(out[currentState]==0) continue;
+            if (out[currentState] == 0) continue;
 
             //Match found, print all matching words of arr[] using
             //output function
-            for(int j=0; j<k; j++){
+            for (int j = 0; j < k; j++) {
 
-                if((out[currentState] & (1<<j))==(1<<j)){
-                    System.out.println("Word "+arr[j]+" appears from "+(i-arr[j].length()+1)+" to "+ i);
+                if ((out[currentState] & (1 << j)) == (1 << j)) {
+                    System.out.println("Word " + arr[j] + " appears from " + (i - arr[j].length() + 1) + " to " + i);
                 }
 
             }
@@ -177,12 +252,13 @@ public class AhoCorasick {
     }
 
 
-    public static void main(String[] args){
-        String[] arr = {"he","she","hers","his"};
+    public static void main(String[] args) {
+        String[] arr = {"he", "she", "hers", "his"};
         String text = "ahishers";
         int k = arr.length;
 
-        searchWords(arr,k,text);
+        searchWords(arr, k, text);
     }
+
 
 }
